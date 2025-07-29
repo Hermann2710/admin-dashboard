98ff87cd4c577d80e4cc1b3a41a48724744f3d10
Hermann2710 <138356589+Hermann2710@users.noreply.github.com>
2025-07-29 18:10:46 +0100

    Correction des middlewares du backend

--- backend/src/middlewares/auth-middleware.ts
+++ backend/src/middlewares/auth-middleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express"
 import jwt from "jsonwebtoken"
 
-const JWT_SECRET = process.env.JWT8SECRET || "secret123"
+const JWT_SECRET = process.env.JWT_SECRET || "secret123"
 
 interface AuthPayload {
   id: string
