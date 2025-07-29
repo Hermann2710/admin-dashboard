d091eb0aeb79c3486e75d8bef9fad8df17d53ed4
Hermann2710 <138356589+Hermann2710@users.noreply.github.com>
2025-07-29 18:00:32 +0100

    12 - Finalisation du prmier deploiement

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://admin-dashboard-w5q4.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
