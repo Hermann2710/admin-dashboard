366c665e66d68e07af5494ef0495cac62a50210e
Hermann2710 <138356589+Hermann2710@users.noreply.github.com>
2025-07-29 01:28:18 +0100

    02 - Configuration du starter kit

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import cors from "cors"
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+
+mongoose
+  .connect(process.env.MONGO_URI || "")
+  .then(() => {
+    console.log("Connexion à Mongo réussie !")
+    app.listen(PORT, () => {
+      console.log(`Serveur en écoute sur le port ${PORT}`)
+    })
+  })
+  .catch((err) => console.error(err))
