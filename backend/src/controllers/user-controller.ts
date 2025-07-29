087fe6e09be25f8a390498c02bfa9430e9b8c506
Hermann2710 <138356589+Hermann2710@users.noreply.github.com>
2025-07-29 08:21:43 +0100

    06 - Route PATCH Admin

--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -29,3 +29,35 @@ export const getAllUsers = async (req: Request, res: Response) => {
     })
   }
 }
+
+export const updateUserRole = async (req: Request, res: Response) => {
+  const userId = req.params.id
+  const { role } = req.body
+
+  if (!["user", "admin"].includes(role)) {
+    return res.status(400).json({
+      message: 'Rôle invalide. Doit être "user" ou "admin".',
+    })
+  }
+
+  try {
+    const user = await User.findById(userId)
+    if (!user)
+      return res.status(404).json({ message: "Utilisateur introuvable ." })
+
+    user.role = role
+    await user.save()
+
+    res
+      .status(200)
+      .json({
+        message: `Role mis à jour en ${role}`,
+        user: { id: user._id, role: user.role },
+      })
+  } catch (error) {
+    res.status(500).json({
+      message: "Erreur serveur .",
+      error: (error as any).message,
+    })
+  }
+}
--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express"
-import { getAllUsers, getMe } from "../controllers/user-controller"
+import {
+  getAllUsers,
+  getMe,
+  updateUserRole,
+} from "../controllers/user-controller"
 import { requireAdmin, requiredAuth } from "../middlewares/auth-middleware"
 
 const router = Router()
 
 router.get("/me", requiredAuth, getMe)
 router.get("/", requiredAuth, requireAdmin, getAllUsers)
+router.patch("/:id/role", requiredAuth, requireAdmin, updateUserRole)
 
 export default router
